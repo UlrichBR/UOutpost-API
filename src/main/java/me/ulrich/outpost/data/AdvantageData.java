5865eaf5765f8e1781d93ce880d0b70162a3fd01
UlrichBR <38494475+UlrichBR@users.noreply.github.com>
2023-06-03 20:22:16 -0300

    Create AdvantageData.java

---
+++ src/main/java/me/ulrich/outpost/data/AdvantageData.java
@@ -0,0 +1,73 @@
+package me.ulrich.outpost.data;
+
+import java.util.List;
+import java.util.UUID;
+
+import me.ulrich.outpost.data.OutpostEnum.AdvantageList;
+
+public class AdvantageData {
+
+	private UUID id;
+	private boolean enabled;
+	private double multiplier;
+	private List<String> whitelist;
+	private AdvantageList type;
+	private UUID outpostUUID;
+
+	public AdvantageData(UUID id, UUID outpostUUID, AdvantageList type, boolean enabled, double multiplier, List<String> whitelist) {
+		this.setId(id);
+		this.setOutpostUUID(outpostUUID);
+		this.setType(type);
+		this.setEnabled(enabled);
+		this.setMultiplier(multiplier);
+		this.setWhitelist(whitelist);
+	}
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public double getMultiplier() {
+		return multiplier;
+	}
+
+	public void setMultiplier(double multiplier) {
+		this.multiplier = multiplier;
+	}
+
+	public List<String> getWhitelist() {
+		return whitelist;
+	}
+
+	public void setWhitelist(List<String> whitelist) {
+		this.whitelist = whitelist;
+	}
+
+	public AdvantageList getType() {
+		return type;
+	}
+
+	public void setType(AdvantageList type) {
+		this.type = type;
+	}
+
+	public UUID getOutpostUUID() {
+		return outpostUUID;
+	}
+
+	public void setOutpostUUID(UUID outpostUUID) {
+		this.outpostUUID = outpostUUID;
+	}
+}
