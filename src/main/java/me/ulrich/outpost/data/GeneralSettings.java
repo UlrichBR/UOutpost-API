a7125ea66d4de66cacbf78885babfd8a8a4361a3
UlrichBR <38494475+UlrichBR@users.noreply.github.com>
2025-02-23 19:23:30 -0300

    Update GeneralSettings.java

--- src/main/java/me/ulrich/outpost/data/GeneralSettings.java
+++ src/main/java/me/ulrich/outpost/data/GeneralSettings.java
@@ -3,9 +3,21 @@ package me.ulrich.outpost.data;
 public class GeneralSettings {
 
 	private boolean allowWithoutGroup;
+	private boolean maintenance;
+	private boolean expelWithoutRequirements;
+	private boolean AbandonInDeath;
+	private boolean abandonIfQuit;
+	private boolean reloadPersistent;
+	private boolean lockCapture;
 
-	public GeneralSettings(boolean allowWithoutGroup) {
+	public GeneralSettings(boolean allowWithoutGroup, boolean maintenance, boolean expelWithoutRequirements, boolean AbandonInDeath, boolean abandonIfQuit, boolean reloadPersistent, boolean lockCapture) {
 		this.setAllowWithoutGroup(allowWithoutGroup);
+		this.setExpelWithoutRequirements(expelWithoutRequirements);
+		this.setMaintenance(maintenance);
+		this.setAbandonInDeath(AbandonInDeath);
+		this.setAbandonIfQuit(abandonIfQuit);
+		this.setReloadPersistent(reloadPersistent);
+		this.setLockCapture(lockCapture);
 	}
 
 	public boolean isAllowWithoutGroup() {
@@ -14,5 +26,53 @@ public class GeneralSettings {
 
 	public void setAllowWithoutGroup(boolean allowWithoutGroup) {
 		this.allowWithoutGroup = allowWithoutGroup;
+	}
+
+	public boolean isMaintenance() {
+		return maintenance;
+	}
+
+	public void setMaintenance(boolean maintenance) {
+		this.maintenance = maintenance;
+	}
+
+	public boolean isExpelWithoutRequirements() {
+		return expelWithoutRequirements;
+	}
+
+	public void setExpelWithoutRequirements(boolean expelWithoutRequirements) {
+		this.expelWithoutRequirements = expelWithoutRequirements;
+	}
+
+	public boolean isAbandonInDeath() {
+		return AbandonInDeath;
+	}
+
+	public void setAbandonInDeath(boolean abandonInDeath) {
+		AbandonInDeath = abandonInDeath;
+	}
+
+	public boolean isAbandonIfQuit() {
+		return abandonIfQuit;
+	}
+
+	public void setAbandonIfQuit(boolean abandonIfQuit) {
+		this.abandonIfQuit = abandonIfQuit;
+	}
+
+	public boolean isReloadPersistent() {
+		return reloadPersistent;
+	}
+
+	public void setReloadPersistent(boolean reloadPersistent) {
+		this.reloadPersistent = reloadPersistent;
+	}
+
+	public boolean isLockCapture() {
+		return lockCapture;
+	}
+
+	public void setLockCapture(boolean lockCapture) {
+		this.lockCapture = lockCapture;
 	}
 }
